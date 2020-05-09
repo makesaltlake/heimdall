4e4c046485d3c9ec213262374578708b90899ff6
Alex Boyd <alex@opengroove.org>
2020-05-08 21:49:14 -0600

    don't store the password, encrypted or otherwise, in paper trail versions

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  # you get versions! and you get versions! everybody gets versions!!!
-  has_paper_trail
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :timeoutable,, :registerable, and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :trackable, :lockable
+
+  has_paper_trail skip: [:password, :password_confirmation, :encrypted_password]
 end
