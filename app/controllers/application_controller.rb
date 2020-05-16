463dcc02ec50b11cb863fabdbff8b8b95da895be
Alex Boyd <alex@opengroove.org>
2020-05-16 08:49:26 -0600

    store the id of the user that made a change in the paper trail version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :set_paper_trail_whodunnit
+
   def admin_access_denied(exception)
     # TODO: pretty this up, maybe tell them to email MSL for help
     render plain: "Sorry, you're not allowed to do that: #{exception.message}", status: :forbidden
