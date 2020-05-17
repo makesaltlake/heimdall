2e61fd7fe75fc42ec8b0c33c61b9e76a381b6842
Alex Boyd <alex@opengroove.org>
2020-05-16 20:47:06 -0600

    disable sql query logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,4 +112,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Disable SQL query logging in production
+  config.after_initialize do
+    ActiveRecord::Base.logger = Rails.logger.clone
+    ActiveRecord::Base.logger.level = Logger::INFO
+  end
 end
