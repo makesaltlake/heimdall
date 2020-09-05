94e1dcdbc865154363c2b8d27cee49eda498bdb8
Alex Boyd <alex@opengroove.org>
2020-09-04 22:49:14 -0600

    pull material-ui into the frontend demo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import BuildIcon from '@material-ui/icons/Build';
+
 const TestComponent = () => {
-  return <div>
-    <div>Hello!</div>
-    <div>How are you?</div>
-    <div>I am fine.</div>
-    <div>(is all my dog will say. He's probably repeated it a thousand times today.)</div>
-  </div>;
+  const [drawerOpen, setDrawerOpen] = useState(true);
+
+  return <>
+    <CssBaseline />
+    <SwipeableDrawer open={drawerOpen}>
+      <div style={{minWidth: '240px'}}>Hi there</div>
+      <Divider />
+      <List>
+        <ListItem>
+          <ListItemIcon><ShoppingCartIcon /></ListItemIcon>
+          <ListItemText>Consumables</ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon><PhotoCameraIcon /></ListItemIcon>
+          <ListItemText>Photo Booth</ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon><BuildIcon /></ListItemIcon>
+          <ListItemText>Admin Site</ListItemText>
+        </ListItem>
+      </List>
+    </SwipeableDrawer>
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6">Heimdall</Typography>
+      </Toolbar>
+    </AppBar>
+    <div>hello there</div>
+  </>;
 };
 
 ReactDOM.render(<TestComponent />, document.getElementById("react-container"));
