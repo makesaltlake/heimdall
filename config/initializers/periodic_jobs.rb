f8063a181921aa4222691457550f74dad90e076a
Alex Boyd <alex@opengroove.org>
2020-10-03 01:16:32 -0600

    move subscription sync time to 5:30am

--- config/initializers/periodic_jobs.rb
+++ config/initializers/periodic_jobs.rb
@@ -1,3 +1,3 @@
-Delayed::Periodic.cron 'Synchronize all Stripe subscriptions', '0 4 * * *' do
+Delayed::Periodic.cron 'Synchronize all Stripe subscriptions', '30 5 * * *' do
   StripeSynchronizationService.sync_all_subscriptions_later
 end
