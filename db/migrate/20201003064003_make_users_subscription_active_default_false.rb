637bcb0706f31b6b84362f2e34aa7afe02087272
Alex Boyd <alex@opengroove.org>
2020-10-03 01:35:09 -0600

    fix migration - apparently `default: false` *is* needed

--- db/migrate/20201003064003_make_users_subscription_active_default_false.rb
+++ db/migrate/20201003064003_make_users_subscription_active_default_false.rb
@@ -1,6 +1,6 @@
 class MakeUsersSubscriptionActiveDefaultFalse < ActiveRecord::Migration[6.0]
   def change
     change_column_default :users, :subscription_active, false
-    change_column_null :users, :subscription_active, false
+    change_column_null :users, :subscription_active, false, default: false
   end
 end
