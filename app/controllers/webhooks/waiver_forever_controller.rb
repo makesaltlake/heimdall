96b41f533008088ccf235a7b2110e328eec99c0d
Alex Boyd <alex@opengroove.org>
2021-01-15 02:52:07 -0700

    clarify that waiverforever webhook pushes are unauthenticated

--- app/controllers/webhooks/waiver_forever_controller.rb
+++ app/controllers/webhooks/waiver_forever_controller.rb
@@ -2,6 +2,9 @@ class Webhooks::WaiverForeverController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def webhook
+    # WaiverForever doesn't appear to provide any decent way to authenticate
+    # the API requests they send, so we extract the ID only and fetch a fresh
+    # copy of the data on our own in the background.
     waiver_id = params[:id]
     Rails.logger.info("Processing WaiverForever webhook for waiver #{waiver_id}")
 
